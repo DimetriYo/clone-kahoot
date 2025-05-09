451d3d4c5c62926a436b24415a5486eeebfe3725
m5production <dimikh@mail.ru>
2025-05-09 20:54:32 +0300

    feat: make image with placeholder

--- src/components/ui/QuestionView.tsx
+++ src/components/ui/QuestionView.tsx
@@ -1,16 +1,17 @@
+import ImgWithPlaceholder from "@/lib/ImgWithPlaceholder"
 import { Question } from "@/types/question"
 
 export function QuestionView({ text, img }: Pick<Question, "img" | "text">) {
-  return img ? (
-    <figure className="flex flex-col items-center gap-4 rounded-lg">
-      <img
+  if (!img) return null
+
+  return (
+    <>
+      <ImgWithPlaceholder
         className="flex flex-col items-center gap-4 rounded-lg"
         src={img}
         alt={`Image illustrating the question: ${text}`}
       />
-      <figcaption className="font-bold text-4xl">{text}</figcaption>
-    </figure>
-  ) : (
-    <p>{text}</p>
+      <p className="font-bold text-4xl">{text}</p>
+    </>
   )
 }
