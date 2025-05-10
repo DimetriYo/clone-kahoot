ef1e1cdd34d86cb4bab24bac8a252d233a4fcaeb
m5production <dimikh@mail.ru>
2025-05-10 13:28:27 +0300

    fix: question view

--- src/components/ui/QuestionView.tsx
+++ src/components/ui/QuestionView.tsx
@@ -2,15 +2,15 @@ import ImgWithPlaceholder from "@/components/ui/ImgWithPlaceholder"
 import { Question } from "@/types/question"
 
 export function QuestionView({ text, img }: Pick<Question, "img" | "text">) {
-  if (!img) return null
-
   return (
     <>
-      <ImgWithPlaceholder
-        className="flex flex-col items-center gap-4 rounded-lg"
-        src={img}
-        alt={`Image illustrating the question: ${text}`}
-      />
+      {img && (
+        <ImgWithPlaceholder
+          className="flex flex-col items-center gap-4 rounded-lg"
+          src={img}
+          alt={`Image illustrating the question: ${text}`}
+        />
+      )}
       <p className="font-bold text-4xl">{text}</p>
     </>
   )
